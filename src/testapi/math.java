4bdf56717efd8815a4f9ba0484d4d791d4e35b86
jenhsiang <chentai008@yahoo.com>
2014-06-28 09:25:03 +0000

    專案初始
    Signed-off-by: jenhsiang <chentai008@yahoo.com>

---
+++ src/testapi/math.java
@@ -0,0 +1,7 @@
+package testapi;
+
+public class math {
+	public int Add(int aa,int bb){
+		return aa+bb;
+	}
+}
---
+++ src/testapi/todo.java
@@ -0,0 +1,11 @@
+package testapi;
+
+public class todo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		math tt = new math();
+		System.out.println(tt.Add(5, 3));
+	}
+
+}
